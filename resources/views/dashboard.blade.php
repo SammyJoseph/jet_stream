c47e119bd262871921129c1c12329d83ec81d1f5
SammyJoseph <sam.tab.paz@gmail.com>
2021-06-04 00:36:58 -0500

    after lesson 27

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Coders Free') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                {{-- <x-jet-welcome /> --}}
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam tempora enim eum doloribus, id vero, rerum inventore odit accusantium dicta, quibusdam temporibus eos alias. Magnam iste dolore ad aperiam delectus.</p>
+                <p>Veritatis perspiciatis provident sapiente accusantium quo eligendi omnis ea amet maxime quaerat. Quidem esse possimus explicabo, minima dicta ad totam debitis voluptates. Culpa magni saepe veniam incidunt consequuntur odio atque!</p>
+                <p>Accusantium, quis temporibus tempore itaque ipsum laborum consequuntur sunt porro dignissimos nisi asperiores rem animi odit illo possimus odio soluta blanditiis vel quam amet autem. Blanditiis et cupiditate laborum incidunt?</p>
+                <p>Enim velit facilis laboriosam, laudantium mollitia delectus! Laudantium totam aut error magnam voluptatum, odit voluptates esse architecto sapiente exercitationem? Praesentium autem iure quidem, ab natus at minima saepe perferendis. Saepe.</p>
+                <p>Quis deleniti itaque voluptas exercitationem modi illum iure officiis ut vitae? Maiores nam provident voluptatum ipsa quia adipisci obcaecati? Consequatur fugiat eveniet quo aliquam soluta laborum enim sint voluptate temporibus.</p>
             </div>
         </div>
     </div>
